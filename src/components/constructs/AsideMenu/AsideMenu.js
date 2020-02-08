05633d1b8ef47b24071f8913c8b9e5a39ad0af4a
Ria N. Carmin <akimalunar@gmail.com>
2020-02-07 17:39:51 -0800

    Implements <AsideMenu /> construct [#5]

--- src/components/constructs/AsideMenu/AsideMenu.js
+++ src/components/constructs/AsideMenu/AsideMenu.js
@@ -1,19 +1,61 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import {
+    brandStyles,
+    homeStyles,
+    iconStyles,
+    linkStyles,
+    logoStyles,
+    rootStyles,
+} from './AsideMenu.styles';
 import { withSpacing } from '@utilities/styles/spacing';
-import { constructStyles } from './AsideMenu.styles';
+import Box from '@elements/Box';
+import BrandLogo from '@constructs/BrandLogo';
+import Logo from '@elements/Logo';
+import Link from '@elements/Link';
+import Text from '@elements/Text';
 
-const AsideMenu = ({ className, ...restProps }) => {
-    const styles = useMemo(() => constructStyles(restProps), [restProps]);
+const AsideMenu = ({ className, links }) => {
     return (
-        <div css={styles} className={className}>
-            AsideMenu
-        </div>
+        <Box py6 css={rootStyles} className={className}>
+            <Link css={linkStyles} to="/">
+                <Box p1 css={[logoStyles, homeStyles]}>
+                    <Logo css={iconStyles} type="mark-inverse" />
+                </Box>
+                <Text mt1 caption>
+                    Home
+                </Text>
+            </Link>
+            {links &&
+                links.map(({ linkBrandName, out, to, title }) => (
+                    <Link
+                        css={linkStyles}
+                        key={linkBrandName}
+                        out={out}
+                        to={to}
+                    >
+                        <Box css={[logoStyles, brandStyles]}>
+                            <BrandLogo css={iconStyles} name={linkBrandName} />
+                        </Box>
+                        <Text mt1 caption>
+                            {title}
+                        </Text>
+                    </Link>
+                ))}
+        </Box>
     );
 };
 
 AsideMenu.propTypes = {
     className: PropTypes.string,
+    links: PropTypes.arrayOf(
+        PropTypes.shape({
+            linkBrandName: PropTypes.string,
+            title: PropTypes.string,
+            to: PropTypes.string,
+            out: PropTypes.string,
+        })
+    ),
 };
 
 export default withSpacing(AsideMenu);
--- src/components/constructs/AsideMenu/AsideMenu.story.js
+++ src/components/constructs/AsideMenu/AsideMenu.story.js
@@ -3,10 +3,27 @@ import { storiesOf } from '@storybook/react';
 import AsideMenu from '@constructs/AsideMenu';
 import notes from './AsideMenu.notes.md';
 
+const links = [
+    { linkBrandName: 'react', out: 'https://reactjs.org/', title: 'React' },
+    {
+        linkBrandName: 'meetup',
+        out: 'https://www.meetup.com/ReactJS-Vancouver-Meetup/',
+        title: 'Meetup',
+    },
+    {
+        linkBrandName: 'slack',
+        out: 'https://slackrv.now.sh/',
+        title: 'Slack',
+    },
+];
 storiesOf('Constructs', module).add(
     'AsideMenu',
     () => (
-        <AsideMenu />
+        <div css={{ height: '100%' }}>
+            <AsideMenu links={links} />
+        </div>
     ),
-    { notes }
+    {
+        notes,
+    }
 );
