41bbac285cb56b180b0bc05b65b2506a3c537002
Jose Munoz <jdmg93@gmail.com>
2020-10-11 11:26:29 -0700

    update unit tests

--- src/components/constructs/Modal/Modal.test.js
+++ src/components/constructs/Modal/Modal.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 
 import Modal from './Modal';
 
+const mockProps = {
+  isOpen: true,
+  onCloseRequest: jest.fn(),
+  children: <span>Hello World!</span>,
+}
+
 describe('Modal', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Modal />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const component = render(<Modal {...mockProps} />);
+
+    expect(component).toBeTruthy();
   });
 });
